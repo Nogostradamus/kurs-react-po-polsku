7a42c8b31ddb37f722c5bb83f6cafced4f879794
krystian <krystian.czekalski@resourcekraft.com>
2019-11-04 15:39:32 +0000

    Gotowa aplikacja

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Zamowienie from './components/zamowienie';
+import Skomponuj from './components/skomponuj';
 
 function App() {
+
+  const [ pizza, setPizza ] = useState(null);
+
+  const otrzymajPizze = pizza => {
+    setPizza(pizza);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="content">
+        <Skomponuj otrzymajPizze={otrzymajPizze}/>
+        <Zamowienie nowaPizza={pizza}/>
+      </div>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Koszyk from './components/koszyk';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MojContext = createContext();
+export const CtxConsumer = MojContext.Consumer;
+const CtxProvider = MojContext.Provider;
+
+function Routing() {
+
+  const [ koszyk, setKoszyk ] = useState(0);
+
+  const odswiez = (dane) => {
+    setKoszyk(dane);
+  }
+
+  return (
+    <BrowserRouter>
+      <CtxProvider value={{koszyk: koszyk, odswiez: odswiez}}>
+        <header className="App-header">
+          <h1>Zamów pizzę</h1>
+        </header>
+        <Route exact path="/" component={App} />
+        <Route exact path="/koszyk" component={Koszyk} />
+        <a href="https://www.freepik.com/free-photos-vectors/food">Food vector created by macrovector - www.freepik.com</a>
+        <br/>
+        <a href="https://www.freepik.com/free-photos-vectors/background">Background photo created by freepik - www.freepik.com</a>
+      </CtxProvider>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
